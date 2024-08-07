08500a5e99d8a2cdc9fe49576f8a52412a6e7c49
PedroFranco-Dev <pedrofranco.dev1@gmail.com>
2024-08-07 12:40:49 -0300

    editing Notes function done

--- js/script.js
+++ js/script.js
@@ -83,6 +83,14 @@ function createNote(id, content, fixed) {
 
     // Elements Events
 
+    element.querySelector("textarea").addEventListener("keyup", (e) => {
+
+        const noteContent = e.target.value
+
+        updateNote(id, noteContent)
+
+    })
+
     element.querySelector(".bi-pin").addEventListener("click", () => {
         toggleFixNote(id);
     });
@@ -135,6 +143,16 @@ function copyNote(id) {
     notesContainer.appendChild(noteElement)
 
     notes.push(noteObject);
+    saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+    const notes = getNotes()
+
+    const targetNote = notes.filter((note) => note.id === id)[0]
+
+    targetNote.content = newContent
+
     saveNotes(notes);
 }
 
