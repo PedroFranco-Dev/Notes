37d0af550357b84caf484fb33248e79563cc3f75
PedroFranco-Dev <pedrofranco.dev1@gmail.com>
2024-08-07 10:43:44 -0300

    Creating Notes function

--- js/script.js
+++ js/script.js
@@ -5,10 +5,38 @@ const addNoteBtn = document.querySelector(".add-note");
 
 
 // Functions
+function addNote() {
+    const noteObject = {
+        id: generateId(),
+        content: noteInput.value,
+        fixed: false,
+    };
 
+    const noteElement = createNote(noteObject.id, noteObject.content);
+
+    notesContainer.appendChild(noteElement);
+}
 
 function generateId() {
     return Math.floor(Math.random() * 5000);
+}
+
+function createNote(id, content, fixed) {
+
+    const element = document.createElement("div")
+    element.classList.add("note")
+
+    const textArea = document.createElement("textarea")
+   
+    textArea.value = content
+
+    textArea.placeholder = "Write any text"
+
+    element.appendChild(textArea)
+
+    return element;
+
+
 }
 
 
